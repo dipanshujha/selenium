2d514324c5cff52db96720ca6cf186bd1c3082b1
Dipanshu Jha <25705132+dipanshujha@users.noreply.github.com>
2019-09-06 19:53:51 +0530

    Create assess.java

---
+++ assess.java
@@ -0,0 +1,13 @@
+package selenium;
+
+public class assess extends testng {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		//assess a = new assess();
+		print("dipanshu");
+		//OpenApp("CH", "www.google.com");
+		open("CH","https://www.google.com");
+	}
+
+}
